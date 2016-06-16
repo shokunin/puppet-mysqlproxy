fca43db08aa483cd1483b90ab1a079fa7cc32b72
Chris Mague <github@mague.com>
2016-06-16 14:19:27 -0700

    test

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,8 +1,16 @@
-class mysqlproxy::service
-{
-  service {
-    'mysql-proxy':
-      ensure    => running,
-      hasstatus => true,
+class mysqlproxy::service {
+
+  include stdlib
+
+  if versioncmp($::os['release']['major'], '14.04') > 0 {
+    notify { 'Setup using systemd': }
+  } else {
+    notify { 'Setup using upstart': }
   }
+
+  service { 'mysql-proxy':
+    ensure    => running,
+    hasstatus => true,
+  }
+
 }
