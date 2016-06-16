691ec0269ec3e883415786d9a38e69cb22be7d09
Chris Mague <github@mague.com>
2016-06-16 14:22:33 -0700

    test

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,11 +2,12 @@ class mysqlproxy::service {
 
   include stdlib
 
-  if versioncmp($::os['release']['major'], '14.04') > 0 {
-    notify { 'Setup using systemd': }
-  } else {
-    notify { 'Setup using upstart': }
-  }
+  notify { "TEST: ${::os['release']['major']}": }
+  #if versioncmp($::os['release']['major'], '14.04') > 0 {
+  #  notify { 'Setup using systemd': }
+  #} else {
+  #  notify { 'Setup using upstart': }
+  #}
 
   service { 'mysql-proxy':
     ensure    => running,
